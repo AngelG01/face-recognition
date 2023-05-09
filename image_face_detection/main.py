ce631e3e531d0564aac212070469fbfd6ffedc18
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-05-09 11:59:39 +0300

    Create main.py

---
+++ image_face_detection/main.py
@@ -0,0 +1,15 @@
+import cv2
+import face_recognition
+
+
+image_to_detect = cv2.imread('C:/Users/Angel/Desktop/test_img2.jpg')
+
+#cv2.imshow('test', image_to_detect)
+
+all_faces_locations = face_recognition.face_locations(image_to_detect, model='hog')
+print(f'Number of faces is {len(all_faces_locations)}')
+
+for index, curr_face_location in enumerate(all_faces_locations):
+    top_pos, right_pos, bottom_pos, left_pos = curr_face_location
+    curr_face_img = image_to_detect[top_pos:bottom_pos, left_pos:right_pos]
+    cv2.imshow(f'{index}', curr_face_img)
